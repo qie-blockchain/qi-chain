cd9487266121b6c30e195e689f9aad469b3b7532
rudransh.sharma <rudransh.sharma@indicchain.com>
2024-01-09 11:06:30 +0530

    changed the connection to internal ip rather than external

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Ethereum provider
-const provider = new ethers.JsonRpcProvider("http://182.72.203.246:10002/");
+const provider = new ethers.JsonRpcProvider("http://172.16.0.228:10002/");
 
 const contractAddress = "0x0000000000000000000000000000000000001001";
 const contractABI = [
